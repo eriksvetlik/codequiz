b8f0bd677ad4df8dcfb0cd54d8b51189adb5ae8c
eriksvetlik <svetlik.erik@gmail.com>
2021-12-28 17:16:46 -0600

    added CSS styling and JavaScript timer

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+var quizArea = document.getElementById("quizArea");
+var startButton = document.getElementById("startButton");
+var timer = document.getElementById("timer");
 
+startButton.addEventListener("click", function () {
+  countdown();
+});
+
+function countdown() {
+  var timeLeft = 61;
+
+  var timeInterval = setInterval(function () {
+    timeLeft--;
+    timer.textContent = "Time: " + timeLeft;
+    question1();
+
+    if (timeLeft === 0) {
+      timer.textContent = "";
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+
+function displayMessage() {
+  alert("Time is over!");
+}
+
+function question1() {
+  quizArea.textContent = "Question 1";
+}
