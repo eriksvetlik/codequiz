3b0ff2dfc8e22c400931bd5f77ccd7df0d1d5e15
eriksvetlik <svetlik.erik@gmail.com>
2022-01-02 13:48:02 -0600

    added comments, removed console.logs, and added additional fixes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var btn4 = document.createElement("button");
 var buttonsID = document.getElementById("buttons");
 var question = document.getElementById("question");
 var questionNumber = document.getElementById("questionNumber");
-var score = 0;
+var score;
 var startButton = document.getElementById("startButton");
 var scoreTable = document.getElementById("scoreTable");
 var scoreValues = [];
@@ -15,8 +15,10 @@ var timeLeft;
 var userInitials;
 var userScores;
 
+// when the user starts the quiz with a button, the timer starts and the first questions is presented
 startButton.addEventListener("click", function () {
   score = 0;
+  timeLeft = 61;
   startButton.style.display = "none";
   question.style.display = "initial";
   btn1.style.display = "initial";
@@ -28,9 +30,8 @@ startButton.addEventListener("click", function () {
   question1();
 });
 
+// when the timer reaches 0, the timer is stopped and the timeUp function is called
 function countdown() {
-  timeLeft = 61;
-
   timeInterval = setInterval(function () {
     timeLeft--;
     timer.textContent = "Time: " + timeLeft;
@@ -43,142 +44,172 @@ function countdown() {
   }, 1000);
 }
 
+// if time still remains, buttons are created for each answer. when the user selects a button, it is compared to the answer.
+// if correct, the user receives 20 points and goes to the next question. if incorrect, 10 seconds is deducted from the time and the user goes to the next question.
 function question1() {
-  questionNumber.textContent = "Question 1";
-  question.textContent = "Commonly used data types DO NOT include:";
+  if (timeLeft > 0) {
+    questionNumber.textContent = "Question 1";
+    question.textContent = "Commonly used data types DO NOT include:";
 
-  btn1.textContent = "strings";
-  btn2.textContent = "booleans";
-  btn3.textContent = "alerts";
-  btn4.textContent = "numbers";
+    btn1.textContent = "strings";
+    btn2.textContent = "booleans";
+    btn3.textContent = "alerts";
+    btn4.textContent = "numbers";
 
-  buttonsID.appendChild(btn1);
-  buttonsID.appendChild(btn2);
-  buttonsID.appendChild(btn3);
-  buttonsID.appendChild(btn4);
+    buttonsID.appendChild(btn1);
+    buttonsID.appendChild(btn2);
+    buttonsID.appendChild(btn3);
+    buttonsID.appendChild(btn4);
 
-  buttonsID.addEventListener("click", function handleAnswer1(event) {
-    var answer = event.target.textContent;
+    buttonsID.addEventListener("click", function handleAnswer1(event) {
+      var answer = event.target.textContent;
 
-    if (answer == btn3.textContent) {
-      alert("Correct!");
-      buttonsID.removeEventListener("click", handleAnswer1);
-      score = score + 20;
-      question2();
-    } else {
-      alert("Incorrect! You've lost 10 seconds!");
-      buttonsID.removeEventListener("click", handleAnswer1);
-      timeLeft = timeLeft - 10;
-      question2();
-    }
-  });
+      if (answer == btn3.textContent) {
+        alert("Correct!");
+        buttonsID.removeEventListener("click", handleAnswer1);
+        score = score + 20;
+        question2();
+      } else {
+        alert("Incorrect! You've lost 10 seconds!");
+        buttonsID.removeEventListener("click", handleAnswer1);
+        timeLeft = timeLeft - 10;
+        question2();
+      }
+    });
+  } else {
+    return;
+  }
 }
 
+// if time still remains, buttons are created for each answer. when the user selects a button, it is compared to the answer.
+// if correct, the user receives 20 points and goes to the next question. if incorrect, 10 seconds is deducted from the time and the user goes to the next question.
 function question2() {
-  questionNumber.textContent = "Question 2";
-  question.textContent =
-    "The condition of an if/else statement is enclosed within:";
+  if (timeLeft > 0) {
+    questionNumber.textContent = "Question 2";
+    question.textContent =
+      "The condition of an if/else statement is enclosed within:";
 
-  btn1.textContent = "quotes";
-  btn2.textContent = "curly brackets";
-  btn3.textContent = "parenthesis";
-  btn4.textContent = "square brackets";
+    btn1.textContent = "quotes";
+    btn2.textContent = "curly brackets";
+    btn3.textContent = "parenthesis";
+    btn4.textContent = "square brackets";
 
-  buttonsID.addEventListener("click", function handleAnswer2(event) {
-    var answer = event.target.textContent;
+    buttonsID.addEventListener("click", function handleAnswer2(event) {
+      var answer = event.target.textContent;
 
-    if (answer == btn3.textContent) {
-      alert("Correct!");
-      buttonsID.removeEventListener("click", handleAnswer2);
-      score = score + 20;
-      question3();
-    } else {
-      alert("Incorrect! You've lost 10 seconds!");
-      buttonsID.removeEventListener("click", handleAnswer2);
-      timeLeft = timeLeft - 10;
-      question3();
-    }
-  });
+      if (answer == btn3.textContent) {
+        alert("Correct!");
+        buttonsID.removeEventListener("click", handleAnswer2);
+        score = score + 20;
+        question3();
+      } else {
+        alert("Incorrect! You've lost 10 seconds!");
+        buttonsID.removeEventListener("click", handleAnswer2);
+        timeLeft = timeLeft - 10;
+        question3();
+      }
+    });
+  } else {
+    return;
+  }
 }
 
+// if time still remains, buttons are created for each answer. when the user selects a button, it is compared to the answer.
+// if correct, the user receives 20 points and goes to the next question. if incorrect, 10 seconds is deducted from the time and the user goes to the next question.
 function question3() {
-  questionNumber.textContent = "Question 3";
-  question.textContent = "How does JavaScript store dates?";
+  if (timeLeft > 0) {
+    questionNumber.textContent = "Question 3";
+    question.textContent = "How does JavaScript store dates?";
 
-  btn1.textContent = "the number of milliseconds since 01/01/1970";
-  btn2.textContent = "the number of hours since 01/01/2000";
-  btn3.textContent = "the number of days since 01/01/1900";
-  btn4.textContent = "none of the above";
+    btn1.textContent = "the number of milliseconds since 01/01/1970";
+    btn2.textContent = "the number of hours since 01/01/2000";
+    btn3.textContent = "the number of days since 01/01/1900";
+    btn4.textContent = "none of the above";
 
-  buttonsID.addEventListener("click", function handleAnswer3(event) {
-    var answer = event.target.textContent;
+    buttonsID.addEventListener("click", function handleAnswer3(event) {
+      var answer = event.target.textContent;
 
-    if (answer === btn1.textContent) {
-      alert("Correct!");
-      buttonsID.removeEventListener("click", handleAnswer3);
-      score = score + 20;
-      question4();
-    } else {
-      alert("Incorrect! You've lost 10 seconds!");
-      buttonsID.removeEventListener("click", handleAnswer3);
-      timeLeft = timeLeft - 10;
-      question4();
-    }
-  });
+      if (answer === btn1.textContent) {
+        alert("Correct!");
+        buttonsID.removeEventListener("click", handleAnswer3);
+        score = score + 20;
+        question4();
+      } else {
+        alert("Incorrect! You've lost 10 seconds!");
+        buttonsID.removeEventListener("click", handleAnswer3);
+        timeLeft = timeLeft - 10;
+        question4();
+      }
+    });
+  } else {
+    return;
+  }
 }
 
+// if time still remains, buttons are created for each answer. when the user selects a button, it is compared to the answer.
+// if correct, the user receives 20 points and goes to the next question. if incorrect, 10 seconds is deducted from the time and the user goes to the next question.
 function question4() {
-  questionNumber.textContent = "Question 4";
-  question.textContent = "Arrays in JavScript can be used to store:";
+  if (timeLeft > 0) {
+    questionNumber.textContent = "Question 4";
+    question.textContent = "Arrays in JavScript can be used to store:";
 
-  btn1.textContent = "numbers and strings";
-  btn2.textContent = "other arrays";
-  btn3.textContent = "booleans";
-  btn4.textContent = "all of the above";
+    btn1.textContent = "numbers and strings";
+    btn2.textContent = "other arrays";
+    btn3.textContent = "booleans";
+    btn4.textContent = "all of the above";
 
-  buttonsID.addEventListener("click", function handleAnswer4(event) {
-    var answer = event.target.textContent;
+    buttonsID.addEventListener("click", function handleAnswer4(event) {
+      var answer = event.target.textContent;
 
-    if (answer === btn4.textContent) {
-      alert("Correct!");
-      buttonsID.removeEventListener("click", handleAnswer4);
-      score = score + 20;
-      question5();
-    } else {
-      alert("Incorrect! You've lost 10 seconds!");
-      buttonsID.removeEventListener("click", handleAnswer4);
-      timeLeft = timeLeft - 10;
-      question5();
-    }
-  });
+      if (answer === btn4.textContent) {
+        alert("Correct!");
+        buttonsID.removeEventListener("click", handleAnswer4);
+        score = score + 20;
+        question5();
+      } else {
+        alert("Incorrect! You've lost 10 seconds!");
+        buttonsID.removeEventListener("click", handleAnswer4);
+        timeLeft = timeLeft - 10;
+        question5();
+      }
+    });
+  } else {
+    return;
+  }
 }
 
+// if time still remains, buttons are created for each answer. when the user selects a button, it is compared to the answer.
+// if correct, the user receives 20 points and the finalScore function is called. if incorrect, the finalScore function is still called.
 function question5() {
-  questionNumber.textContent = "Question 5";
-  question.textContent = "Inside what type of element do you put JavaScript?";
+  if (timeLeft > 0) {
+    questionNumber.textContent = "Question 5";
+    question.textContent = "Inside what type of element do you put JavaScript?";
 
-  btn1.textContent = "<js>";
-  btn2.textContent = "<script>";
-  btn3.textContent = "<javascript>";
-  btn4.textContent = "none of the above";
+    btn1.textContent = "<js>";
+    btn2.textContent = "<script>";
+    btn3.textContent = "<javascript>";
+    btn4.textContent = "none of the above";
 
-  buttonsID.addEventListener("click", function handleAnswer5(event) {
-    var answer = event.target.textContent;
+    buttonsID.addEventListener("click", function handleAnswer5(event) {
+      var answer = event.target.textContent;
 
-    if (answer === btn2.textContent) {
-      alert("Correct!");
-      buttonsID.removeEventListener("click", handleAnswer5);
-      score = score + 20;
-      finalScore();
-    } else {
-      alert("Incorrect! You've lost 10 seconds!");
-      buttonsID.removeEventListener("click", handleAnswer5);
-      timeLeft = timeLeft - 10;
-      finalScore();
-    }
-  });
+      if (answer === btn2.textContent) {
+        alert("Correct!");
+        buttonsID.removeEventListener("click", handleAnswer5);
+        score = score + 20;
+        finalScore();
+      } else {
+        alert("Incorrect!");
+        buttonsID.removeEventListener("click", handleAnswer5);
+        finalScore();
+      }
+    });
+  } else {
+    return;
+  }
 }
 
+// the user receives their final score and gets a prompt to enter their initials to save the score. Once saved, the allScores function is called.
 function finalScore() {
   questionNumber.textContent = "";
   question.textContent = "";
@@ -199,6 +230,7 @@ function finalScore() {
   allScores();
 }
 
+// if the timer runs to 0, the user receives their final score and gets a prompt to enter their initials to save the score. Once saved, the allScores function is called.
 function timeUp() {
   questionNumber.textContent = "";
   question.textContent = "";
@@ -214,13 +246,14 @@ function timeUp() {
   );
   if (initials === "") {
     alert("Please enter at least one letter");
-    finalScore();
+    timeUp();
     return;
   }
   scoreValues.push(initials + ": " + score);
   allScores();
 }
 
+// the user is able to view all recorded scores and retake the quiz
 function allScores() {
   timer.textContent = "";
   clearInterval(timeInterval);
@@ -233,7 +266,6 @@ function allScores() {
   startButton.style.display = "initial";
   scoreTable.style.display = "initial";
   for (var i = 0; i < scoreValues.length; i++) {
-    console.log(scoreValues);
     scoreTable.innerHTML = scoreValues.join("<br/>");
   }
   startButton.textContent = "Retake Quiz";
